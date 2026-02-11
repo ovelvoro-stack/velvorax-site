755e75a2650d8e910bb1e4ec70c6a9340249e0d6
Velvoro Software Solutions <ovelvoro@gmail.com>
2026-02-11 22:55:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Home Page
 @app.route("/")
 def home():
     return render_template("index.html")
 
+# About Page
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+# Services Page
 @app.route("/services")
 def services():
     return render_template("services.html")
 
+# Contact Page
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
 
+# Pricing Page
+@app.route("/pricing")
+def pricing():
+    return render_template("pricing.html")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
