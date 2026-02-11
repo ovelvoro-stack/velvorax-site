7df51f5c2871f915d62d9480027edab439607df6
Velvoro Software Solutions <ovelvoro@gmail.com>
2026-02-11 22:58:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,27 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Home Page
 @app.route("/")
 def home():
     return render_template("index.html")
 
-# About Page
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-# Services Page
 @app.route("/services")
 def services():
     return render_template("services.html")
 
-# Contact Page
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
 
-# Pricing Page
 @app.route("/pricing")
 def pricing():
     return render_template("pricing.html")
